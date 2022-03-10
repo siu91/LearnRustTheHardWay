2d0ea02b727201794031825efb2a876c685bb709
Siu <gshiwen@gmail.com>
2022-03-10 22:02:13 +0800

    study：熟悉基本语法

--- code/main/src/main.rs
+++ code/main/src/main.rs
@@ -11,10 +11,24 @@ fn hello_world() {
 // 熟悉基本的数据类型
 fn data_types() {
     // integer 整型
-    let int32 = 3; // 默认就是 i32，整形 32
-    let int32_1: i32 = 3;// 也可以这样定义
-    let int32_2 = 3_i32;// 还可以这样定义
-    println!("{}",3)
+    let int1 = 3; // 默认就是 i32，整形 32
+    let int2: i32 = 3;// 也可以这样定义
+    let int3 = 3_i32;// 还可以这样定义
+    let int4 = 3i32;// 还可以这样定义
+    println!("{}/{}/{}/{}", int1, int2, int3, int4);
+    // float 浮点型
+    let float1 = 3.1;
+    let float2: f32 = 3.1;
+    assert_eq!(float1, float2);
+
+    // range 打印1-4，有点像 python是不是
+    for i in 1..5{
+        println!("{}",i)
+    }
+    // 打印 1-5
+    for i in 1..=5{
+        println!("{}",i)
+    }
 }
 // ANCHOR_END: data_types
 
--- src/1-into-rust/hello-world.md
+++ src/1-into-rust/hello-world.md
@@ -6,3 +6,10 @@
 	hello_world();
 {{#include ../../code/main/src/.template:3:3}}
 ```
+
+```rust
+{{#include ../../code/main/src/main.rs:data_types}}
+{{#include ../../code/main/src/.template:1:2}}
+	data_types();
+{{#include ../../code/main/src/.template:3:3}}
+```
