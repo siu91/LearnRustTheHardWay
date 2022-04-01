5c4e55937f55634764af3c18e94ff7ac45ca0a29
Siu <gshiwen@gmail.com>
2022-04-01 11:16:39 +0800

    leetcode：两数之和

--- code/main/src/main.rs
+++ code/main/src/main.rs
@@ -175,7 +175,7 @@ fn move_ownership1() {
     let x = 5;
     let y = x;
 
-    println!("x={},y={}",x,y)
+    println!("x={},y={}", x, y)
 }
 // ANCHOR_END: move_ownership1
 
@@ -184,7 +184,7 @@ fn move_ownership2() {
     let s1 = "s111";
     let s2 = s1;
 
-    println!("s1={},s2={}",s1,s2)
+    println!("s1={},s2={}", s1, s2)
 }
 // ANCHOR_END: move_ownership2
 
@@ -193,12 +193,162 @@ fn move_ownership3() {
     let s1 = String::from("s111");
     let s2 = s1;
 
-    println!("s1={},s2={}",s1,s2)
+    //println!("s1={},s2={}", s1, s2)
 }
 // ANCHOR_END: move_ownership3
 
 
+fn super_add1<T: std::ops::Add<Output=T>>(a: T, b: T) -> T {
+    a + b
+}
+
+
+// ANCHOR: trait
+trait Hello {
+    fn say_hi(&self) -> String {
+        String::from("hi")
+    }
+
+    fn say_something(&self) -> String;
+}
+
+struct Student {}
+
+struct Teacher {}
+
+
+impl Hello for Student {
+    fn say_something(&self) -> String {
+        "i am a Student".to_string()
+    }
+}
+
+impl Hello for Teacher {
+    fn say_hi(&self) -> String {
+        String::from("hello")
+    }
+
+    fn say_something(&self) -> String {
+        "i am a teacher".to_string()
+    }
+}
+
+
+// ANCHOR_END: trait
+
+
+// ANCHOR: hashmap
+use std::collections::HashMap;
+
+fn word_count() {
+    let text = "i am Rust developer,i love Rust";
+    let mut counter = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = counter.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", counter)
+}
+// ANCHOR_END: hashmap
+
+// ANCHOR：panic
+
+use std::net::IpAddr;
+
+fn panic_unwrap() {
+    let ip: IpAddr = "127.0.0.1".parse().unwrap();
+}
+
+// ANCHOR_END：panic
+
+
+use std::fs::File;
+use std::io::ErrorKind;
+
+
+fn open_file() {
+    let f = File::open("Cargo1.toml");
+    let f = match f {
+        Ok(file) => {
+            println!("open file success");
+            file
+        }
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("Cargo1.toml") {
+                Ok(f1) => {
+                    println!("create file");
+                    f1
+                }
+                Err(e) => panic!("create file error")
+            },
+            oe => panic!("open file error:{:?}", oe)
+        }
+    };
+    println!("s");
+
+
+    let f2 = File::open("Cargo1.toml").expect("can not open file");
+}
+
+fn print_something(s: &'static str) {
+    println!("some:{}", s);
+}
+
+fn iters() {
+    let values = vec![1, 2, 3];
+
+    for v in values.iter() {
+        println!("v={}", v)
+    }
+
+    println!("{:?}", values);
+
+
+    let values = vec![1, 2, 3];
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let length = nums.len();
+        return if length >= 2 {
+            for i in 0..length - 1 {
+                for j in 0..=length - 1 {
+                    if i == j {
+                        continue;
+                    }
+                    if nums.get(j).unwrap() + nums.get(i).unwrap() == target {
+                        return vec![i as i32, j as i32];
+                    }
+                }
+            }
+            vec![-1, -1]
+        } else {
+            vec![-1, -1]
+        };
+    }
+}
+
+
 fn main() {
+    let nums = vec![3, 3];
+    let target = 6;
+    let s = Solution::two_sum(nums, target);
+
+    let nums = vec![2, 5, 5, 11];
+    let target = 10;
+    let s = Solution::two_sum(nums, target);
+
+    let nums = vec![3, 2, 4];
+    let target = 6;
+    let s = Solution::two_sum(nums, target);
+
+
+    let nums = vec![-1, -2, -3, -4, -5];
+    let target = -8;
+    let s = Solution::two_sum(nums, target);
+
     hello_world();
     data_types();
     some_syntax();
@@ -209,7 +359,18 @@ fn main() {
     tuple_type();
     move_ownership1();
     move_ownership2();
-    move_ownership3();
+    //move_ownership3();
+    super_add1(1.1, 1.3);
+
+    let s = Student {};
+    s.say_hi();
+    s.say_something();
+    word_count();
+    open_file();
+
+    let s = "hiiiii";
+    print_something(s);
+    iters()
 }
 
 
