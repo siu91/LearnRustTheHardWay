ef25c26c931b906a76977820e45da70becd864a1
Siu <gshiwen@gmail.com>
2022-04-04 21:04:10 +0800

    Update main.rs

--- code/main/src/main.rs
+++ code/main/src/main.rs
@@ -311,27 +311,209 @@ struct Solution {}
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let length = nums.len();
-        return if length >= 2 {
-            for i in 0..length - 1 {
-                for j in 0..=length - 1 {
-                    if i == j {
-                        continue;
-                    }
-                    if nums.get(j).unwrap() + nums.get(i).unwrap() == target {
-                        return vec![i as i32, j as i32];
-                    }
+        // 算法思路：
+        // 输入     nums = [2,7,11,15], target = 9
+        // 得到差值  subs = [7,2,-3,-6]  带上下标表示 subMap = {<7,0>,<2,1>,<-3,2>,<-6,3>}
+        // 1、用 Map<sub,index> 保存每个数的差值和下标
+        // 2、遍历数组：如果 Map 中查找匹配到了当前"差值"，直接返回两个数的索引下标，否则保存"差值和下标"
+        let mut map = HashMap::with_capacity(nums.len());
+        for i in 0..nums.len() {
+            let sub = target - nums[i];
+            if let Some(s) = map.get(&sub) {
+                if *s != i as i32 {
+                    return vec![*s, i as i32];
+                }
+            };
+
+            map.insert(sub, i as i32);
+        }
+        vec![-1, -1]
+    }
+
+    pub fn two_sum1(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for i in 0..nums.len() {
+            for j in 0..nums.len() {
+                if i != j &&
+                    nums[j] + nums[i] == target {
+                    return vec![i as i32, j as i32];
                 }
             }
-            vec![-1, -1]
+        }
+        vec![-1, -1]
+    }
+}
+
+//Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub next: Option<Box<ListNode>>,
+    val: i32,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode {
+            next: None,
+            val,
+        }
+    }
+}
+
+impl Solution {
+    pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut head = None;
+        let mut tail = &mut head;
+
+        let mut t = (l1, l2, 0, 0); // (l1,l2,sum,carry)
+        loop {
+            t = match t {
+                // 遍历完两个链表为空，sum=any，carry=0（无进位）；退出
+                (None, None, _, 0) => break,
+                // 遍历完两个链表为空，还有进位，把进位放在 sum 位置，用于初始化下一个节点（补1）
+                (None, None, _, carry) => (None, None, carry, 0),
+                // 任意链表为空的情况（1个链表已经遍历完了），但有进位，list.next、None 可以放在1和2任意位置
+                (Some(list), None, _, carry) | (None, Some(list), _, carry) => {
+                    let s = if list.val + carry >= 10 { list.val + carry - 10 } else { list.val + carry };
+                    let c = if list.val + carry >= 10 { 1 } else { 0 };
+                    (list.next, None, s, c)
+                }
+                // 正常遍历链表，计算 sum 和 carry
+                (Some(l1), Some(l2), _, carry) => {
+                    let s1 = l1.val + l2.val + carry;
+                    let s = if s1 >= 10 { s1 - 10 } else { s1 };
+                    let c = if s1 >= 10 { 1 } else { 0 };
+                    (l1.next, l2.next, s, c)
+                }
+            };
+            // 处理 tail
+            /*if t.0 != None {
+                println!("ttttt1:{:?}",t.0.as_ref().unwrap().val);
+            }
+
+            if t.1 != None {
+                println!("ttttt2:{:?}",t.1.as_ref().unwrap().val);
+            }*/
+
+            // 把 sum(t.2) 用于初始化当前节点
+            *tail = Some(Box::new(ListNode::new(t.2)));
+            // next
+            //tail = &mut tail.next;
+            tail = &mut tail.as_mut().unwrap().next;
+        }
+
+        return head;
+    }
+}
+
+use std::collections::HashSet;
+use std::cmp::max;
+
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        // 算法思路：
+        // 输入 "anviaj"
+        // 转化 字符串转成 char 数组 [a,n,v,i,a,j]
+        // 窗口每次滑动1 [a,n,v,i,a,j] [n,v,i,a,j] [v,i,a,j] [i,a,j] ...
+
+        // 1、全局定义一个 longest 用于max 每个窗口的最长子串
+        // 2、从 i =0 开始滑动遍历数组：用 start 标记每个窗口内最长的子串（滑动的轨迹），HashSet 记录窗口内重复的字符串
+
+        let mut longest = 0;
+        let s: Vec<_> = s.chars().collect(); // anviaj
+
+        for i in 0..s.len() {
+            // 用 start 去标记滑动的位置，set 记录窗口内重复的字符串
+            let mut set = HashSet::with_capacity(s.len());
+            let mut start = i;
+            while start < s.len() {
+                if !set.contains(&s[start]) {
+                    set.insert(s[start]);
+                    start += 1;
+                } else { break; }
+            }
+            longest = max(longest, start - i);
+        }
+
+        longest as i32
+    }
+}
+
+use std::cmp::min;
+
+impl Solution {
+    pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+        // nums1 = [1,3], nums2 = [2]
+        let len = nums1.len() + nums2.len();
+        let mut i = -1;
+        let mut j = -1;
+        let mut nums = Vec::with_capacity(len);
+        if len % 2 == 0 {
+            i = (len / 2) - 1;
+            j = len / 2;
         } else {
-            vec![-1, -1]
+            j = ((len + 1) / 2) - 1
+        }
+
+        //            while
+
+        for m in 0..len {
+            let n = m;
+            while m < nums1.len() && m < nums2.len() {
+                if nums1[m] < nums2[m] {
+                    nums[m] = nums1[m];
+                } else {
+                    nums[m] = nums2[m];
+                }
+            }
+        }
+
+
+        return 1f64;
+    }
+    pub fn find_median_sorted_arrays2(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+        let mut nums: Vec<i32> = Vec::with_capacity(nums1.len() + nums2.len());
+        //nums1.as_ref().iter_mut().map(|_,x|(nums.push(x)));
+        for n in nums1 {
+            nums.push(n);
+        }
+        for n in nums2 {
+            nums.push(n);
+        }
+        nums.sort();
+        println!("{}", 111);
+        return if nums.len() % 2 == 0 {
+            ((nums[(nums.len() / 2) - 1] as f64 + nums[nums.len() / 2] as f64) / 2f64) as f64
+        } else {
+            nums[((nums.len() + 1) / 2) - 1] as f64
         };
     }
 }
 
 
 fn main() {
+    let m = Solution::find_median_sorted_arrays(vec![1, 3], vec![2]);
+    assert_eq!(m, 2f64);
+    let m = Solution::find_median_sorted_arrays(vec![1, 2], vec![3, 4]);
+    assert_eq!(m, 2.5f64);
+
+    let s = Solution::length_of_longest_substring(String::from("aaaaabc"));
+    assert_eq!(s, 3);
+    let s = Solution::length_of_longest_substring(String::from("abcadbcbb"));
+    assert_eq!(s, 4);
+
+    let s = Solution::length_of_longest_substring(String::from("bbbbb"));
+    assert_eq!(s, 1);
+
+    let s = Solution::length_of_longest_substring(String::from("pwwkew"));
+    assert_eq!(s, 3);
+
+    let s = Solution::length_of_longest_substring(String::from("dvdf"));
+    assert_eq!(s, 3);
+
+    let s = Solution::length_of_longest_substring(String::from("anviaj"));
+    assert_eq!(s, 5);
+
     let nums = vec![3, 3];
     let target = 6;
     let s = Solution::two_sum(nums, target);
@@ -347,6 +529,10 @@ fn main() {
 
     let nums = vec![-1, -2, -3, -4, -5];
     let target = -8;
+    let s = Solution::two_sum(nums, target);
+
+    let nums = vec![3];
+    let target = 6;
     let s = Solution::two_sum(nums, target);
 
     hello_world();
@@ -370,7 +556,48 @@ fn main() {
 
     let s = "hiiiii";
     print_something(s);
-    iters()
+    iters();
+
+    let mut map = HashMap::with_capacity(3);
+    map.insert(1, 7);
+    map.insert(2, 2);
+
+    let v = Some(8i32);
+    let kk = 1;
+
+    if let Some(k) = map.get(&kk) {
+        if *k == 7 {
+            println!("yes")
+        }
+        println!("{}", *k);
+        println!("{}", k);
+        println!("s")
+    }
+
+
+    //输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+    //输出：[8,9,9,9,0,0,0,1]
+
+    let n1 = ListNode::new(9);
+    let n1 = ListNode { val: 8, next: Some(Box::new(n1)) };
+    let n1 = ListNode { val: 7, next: Some(Box::new(n1)) };
+    let n1 = ListNode { val: 6, next: Some(Box::new(n1)) };
+    let n1 = ListNode { val: 5, next: Some(Box::new(n1)) };
+    let n1 = ListNode { val: 4, next: Some(Box::new(n1)) };
+    let n1 = ListNode { val: 3, next: Some(Box::new(n1)) };
+    let n1 = ListNode { val: 2, next: Some(Box::new(n1)) };
+    let mut l1 = Some(Box::new(n1));
+
+
+    // 1->2->2->4->3->5
+    let n = ListNode::new(9);
+    let n = ListNode { val: 9, next: Some(Box::new(n)) };
+    let n = ListNode { val: 9, next: Some(Box::new(n)) };
+    let n = ListNode { val: 9, next: Some(Box::new(n)) };
+    let l2 = Some(Box::new(n));
+
+
+    Solution::add_two_numbers(l1, l2);
 }
 
 
